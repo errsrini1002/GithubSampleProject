ef5dfb41ec5ecaba8ad4d40ec1b8a276c03b0758
krishna <errsrini1001@gmail.com>
2023-10-20 17:23:25 +0100

    first commit

---
+++ src/test/java/com/bt/githubprj/GithubSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bt.githubprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
